366752b44d3218b367d82d63ddc0465178800db4
pedrogaspar88@gmail.com <pedrogaspar88@gmail.com>
2021-01-20 18:39:59 +0000

    Add project files.

---
+++ site_pessoal_dinamico/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace site_pessoal_dinamico.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
